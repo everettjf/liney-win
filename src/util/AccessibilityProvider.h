adfc79aeeb418e226a44ec1d953c5f119e506a59
Everett <everettjf@live.com>
2026-07-21 19:32:32 +0800

    Restore UI Automation header content

--- src/util/AccessibilityProvider.h
+++ src/util/AccessibilityProvider.h
@@ -1,1 +1,11 @@
-@src/util/AccessibilityProvider.h
+#pragma once
+
+#include <windows.h>
+#include <unknwn.h> // COM's `interface` macro required by generated UIA headers
+#include <UIAutomation.h>
+
+namespace liney {
+
+IRawElementProviderSimple* createAccessibilityProvider(HWND hwnd);
+
+} // namespace liney
